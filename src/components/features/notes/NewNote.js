ac29c0aeb253330c965226fb7bcfe4ccbcbebb29
Severin Dragos <severin.dragos@gmail.com>
2020-10-05 15:16:35 +0300

    added select css

--- src/components/features/notes/NewNote.js
+++ src/components/features/notes/NewNote.js
@@ -1,40 +1,49 @@
 import React, { useCallback, useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 
 const NewNote = (props) => {
   const [displayForm, setDisplayForm] = useState(false);
 
   const displayHandler = useCallback(() => {
-    setDisplayForm(true);
-  }, [setDisplayForm]);
+    setDisplayForm(!displayForm);
+  }, [setDisplayForm, displayForm]);
+
+  const buttonText = displayForm ? "Hide" : "Add Note";
 
   const jsx = displayForm ? (
     <>
       <label>Category</label>
       <br />
-      <select>
-        <option value="grapefruit">Grapefruit</option>
-        <option value="lime">Lime</option>
-        <option selected value="coconut">
-          Coconut
-        </option>
-        <option value="mango">Mango</option>
-      </select>
+      <div class="selectdiv">
+        <select>
+          <option value="grapefruit">Grapefruit</option>
+          <option value="lime">Lime</option>
+          <option value="coconut">
+            Coconut
+          </option>
+          <option value="mango">Mango</option>
+        </select>
+      </div>
+      <br />
+      <br />
       <br />
       <label>Text</label>
       <br />
-      <textarea style={{width: "60%", height: "200px"}}></textarea>
+      <textarea style={{ width: "60%", height: "200px" }}></textarea>
+      <br />
       <br />
       <Button variant="primary">Save</Button>
     </>
   ) : (
-    <Button variant="primary" onClick={displayHandler}>
-      Add Note
-    </Button>
+    ""
   );
 
   return (
     <Container>
+      <Button variant="primary" onClick={displayHandler}>
+        {buttonText}
+      </Button>
+      <br />
       {jsx}
       <hr />
     </Container>
